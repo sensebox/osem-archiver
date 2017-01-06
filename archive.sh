697597e27121fd9d44479f41fd0334a5dc018d76
ubergesundheit <ubergesundheit@googlemail.com>
2017-01-06 23:22:34 +0100

    put jq into helpers, add info json

--- archive.sh
+++ archive.sh
@@ -35,15 +35,22 @@ ARCHIVE_TO=$(printf %s "$FOLDER_NAME" "T23:59:59.999Z")
 dav_mkdir "$FOLDER_NAME"
 
 # query boxes from database
-BOXES_RAW=$(mongo_export -c boxes --jsonArray -f sensors)
+BOXES_RAW=$(mongo_export -c boxes --jsonArray -f sensors,name,boxType,exposure,model,loc)
 
 # extract just the ids from $BOXES_RAW then
-echo "$BOXES_RAW" | jq -r '.[]._id | .["$oid"]' | while read -r boxid ; do
+echo "$BOXES_RAW" | jq_boxids | while read -r boxid ; do
+  # extract box name (id-name) from $BOXES_RAW
+  BOX_NAME=$(echo "$BOXES_RAW" | jq_box_name "$boxid")
+
   # create folder for this box
-  dav_mkdir "$FOLDER_NAME/$boxid"
+  dav_mkdir "$FOLDER_NAME/$BOX_NAME"
+
+  # create json file with box information
+  echo "$BOXES_RAW" | jq_box_json "$boxid" | dav_upload "$FOLDER_NAME/$BOX_NAME/$BOX_NAME.json"
+
   # iterate over sensor ids
-  echo "$BOXES_RAW" | jq -r ".[] | select((._id | .[\"\$oid\"])==\"$boxid\") | .sensors[]._id | .[\"\$oid\"]" | while read -r sensor_id ; do
-    mongo_export -c measurements --fields createdAt,value --type csv --query "{sensor_id: ObjectId(\"$sensor_id\"), createdAt: { \$gte: new Date(\"$ARCHIVE_FROM\"), \$lte: new Date(\"$ARCHIVE_TO\") } }" | dav_upload "$FOLDER_NAME/$boxid/$sensor_id.csv"
+  echo "$BOXES_RAW" | jq_box_sensorids "$boxid" | while read -r sensor_id ; do
+    mongo_export -c measurements --fields createdAt,value --type csv --query "{sensor_id: ObjectId(\"$sensor_id\"), createdAt: { \$gte: new Date(\"$ARCHIVE_FROM\"), \$lte: new Date(\"$ARCHIVE_TO\") } }" | dav_upload "$FOLDER_NAME/$BOX_NAME/$sensor_id.csv"
   done
 done
 
--- helpers.sh
+++ helpers.sh
@@ -8,7 +8,8 @@ dav_mkdir () {
   then
     echo "-dav_mkdir: Parameter #1 is zero length.-"
   else
-    curl --user "$DAV_USER:$DAV_PASS" "$DAV_URL/$ARCHIVE_FOLDER/$1" -XMKCOL
+    echo "Creating folder $1 .."
+    curl --fail --user "$DAV_USER:$DAV_PASS" "$DAV_URL/$ARCHIVE_FOLDER/$1" -XMKCOL
   fi
 }
 
@@ -17,10 +18,42 @@ dav_upload () {
   then
     echo "-dav_upload: Parameter #1 is zero length.-"
   else
-    curl --user "$DAV_USER:$DAV_PASS" "$DAV_URL/$ARCHIVE_FOLDER/$1" -XPUT --upload-file -
+    echo "Uploading file $1 .."
+    curl --fail --user "$DAV_USER:$DAV_PASS" "$DAV_URL/$ARCHIVE_FOLDER/$1" -XPUT --upload-file -
   fi
 }
 
 mongo_export () {
   mongoexport -h "$MONGO_HOST" -d "$MONGO_DB" -u "$MONGO_USER" -p "$MONGO_PASS" --quiet "$@"
+}
+
+jq_boxids () {
+  jq -r '.[]._id | .["$oid"]'
+}
+
+jq_box_json () {
+  if [ -z "$1" ]
+  then
+    echo "-jq_box_json: Parameter #1 is zero length.-"
+  else
+    jq -r -c -M ".[] | select((._id | .[\"\$oid\"])==\"$1\") | {name,id: (._id | .[\"\$oid\"]),boxType,exposure,model,loc: {geometry:.loc[0].geometry}, sensors: [(.sensors[] | {title, unit, sensorType, id: (._id | .[\"\$oid\"])})] }"
+  fi
+}
+
+jq_box_sensorids () {
+  if [ -z "$1" ]
+  then
+    echo "-jq_box_sensorids: Parameter #1 is zero length.-"
+  else
+    jq -r ".[] | select((._id | .[\"\$oid\"])==\"$1\") | .sensors[]._id | .[\"\$oid\"]"
+  fi
+}
+
+jq_box_name () {
+  if [ -z "$1" ]
+  then
+    echo "-jq_box_name: Parameter #1 is zero length.-"
+  else
+    jq -r ".[] | select((._id | .[\"\$oid\"])==\"$1\") | [(._id | .[\"\$oid\"]), \"-\", .name] | add" | sed -e 's/[^A-Za-z0-9._-]/_/g'
+  fi
 }
